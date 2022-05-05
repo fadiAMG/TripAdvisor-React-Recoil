2edfa7fbeaa9f5a1fe24fbe326ed9209413d0582
fadi amged <fadiamged780@gmail.com>
2022-05-05 08:36:45 +0200

    :zap: added filter by location type

--- src/Recoil/Selectors/PlacesSelector.js
+++ src/Recoil/Selectors/PlacesSelector.js
@@ -1,29 +1,31 @@
 import axios from "axios";
 import { selectorFamily } from "recoil";
-
-const URL =
-  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+import { filterType } from "../Atoms/FilterType";
 
 export const currentPlaces = selectorFamily({
   key: "CurrentPlaces",
   get:
-    ({ ne, sw }) =>
+    (boundries) =>
     async ({ get }) => {
+      const filter = get(filterType);
       const {
         data: { data },
-      } = await axios.get(URL, {
-        params: {
-          bl_latitude: sw.lat,
-          tr_latitude: ne.lat,
-          bl_longitude: sw.lng,
-          tr_longitude: ne.lng,
-        },
-        headers: {
-          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-          "X-RapidAPI-Key":
-            "a21d35cbcemsh770cb925ca72f39p150a42jsn835a9e55a28f",
-        },
-      });
+      } = await axios.get(
+        `https://travel-advisor.p.rapidapi.com/${filter}/list-in-boundary`,
+        {
+          params: {
+            bl_latitude: boundries.sw.lat,
+            tr_latitude: boundries.ne.lat,
+            bl_longitude: boundries.sw.lng,
+            tr_longitude: boundries.ne.lng,
+          },
+          headers: {
+            "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+            "X-RapidAPI-Key":
+              "a21d35cbcemsh770cb925ca72f39p150a42jsn835a9e55a28f",
+          },
+        }
+      );
 
       return data;
     },
